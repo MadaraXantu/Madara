5330316e86a12b9f39dd8a30fa114f74c5810f8e
MadaraXantu <chowdhuryneha201@gmail.com>
2026-04-26 16:19:27 +0600

    Update pending.js

--- scripts/cmds/pending.js
+++ scripts/cmds/pending.js
@@ -29,7 +29,7 @@ module.exports = {
     const input = event.body.trim();
     const { threadID, messageID } = event;
     const prefix = global.GoatBot?.config?.prefix || "/";
-    const botNickname = "💋♡χα∂ιкα вαву♡💌🦋 くめ";
+    const botNickname = "💋Y𝐨𝐮'𝐫 P𝐨𝐤𝐤𝐢𝐞🐱^᪲᪲᪲✨";
     let done = 0;
 
     const dateTime = moment()
@@ -49,7 +49,7 @@ module.exports = {
 `╭─🚫 ACCESS DENIED 🚫─╮
 │ 🤖 Bot : Refused
 │ 🔗 Prefix : ${prefix}
-│ ⚡ Owner : xalman
+│ ⚡ Owner : Antu
 │ ⏰ Date/Time : ${dateTime}
 ╰──────────────────╯`,
           targetThreadID
@@ -78,7 +78,7 @@ module.exports = {
 `╭─✨ SYSTEM GOAT ✨─╮
 │ 🤖 Bot : Activated
 │ 🔗 Prefix : ${prefix}
-│ ⚡ Owner : xalman
+│ ⚡ Owner : Antu
 │ ⏰ Date/Time : ${dateTime}
 ╰─✅ Access Granted─╯`,
         targetThreadID
